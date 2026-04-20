88390325b410a976de4f20554c7ff9fdb23f5db3
phamhoangviet261 <hoangviet.it.hcmus@gmail.com>
2026-04-20 22:44:18 +0700

    migrate

--- starter-kit/next.config.mjs
+++
@@ -1,7 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  // See https://lucide.dev/guide/packages/lucide-react#nextjs-example
-  transpilePackages: ["lucide-react"],
-}
-
-export default nextConfig
--- starter-kit/src/app/(dashboard-layout)/layout.tsx
+++
@@ -1,9 +0,0 @@
-import { Layout } from "@/components/layout"
-
-export default async function DashboardLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
-  return <Layout>{children}</Layout>
-}
